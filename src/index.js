1b9c35938ffae02739e28dfb3ce433f0628452da
tehkum kanchwala <tehkumkanchwala@gmail.com>
2023-08-13 14:48:22 +0530

    strictmode removed

--- src/index.js
+++ src/index.js
@@ -10,13 +10,13 @@ export { useItems };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  // <React.StrictMode>
     <Router>
       <ItemContext>
         <App />
       </ItemContext>
     </Router>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
